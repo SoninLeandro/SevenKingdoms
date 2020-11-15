1c9305ade29796c785ca8d66588b69d601fd6153
Leandro <soninleandro@gmail.com>
2020-11-15 19:49:00 -0300

    Updates

---
+++ precache-manifest.cfbba5bb0ce40d39881d88f035885103.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41399bb0bf0b8865ddac14bc5786336",
+    "url": "/SevenKingdoms/index.html"
+  },
+  {
+    "revision": "e4bf918d69cbec6f5f7f",
+    "url": "/SevenKingdoms/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0867639c91b94aedddd6",
+    "url": "/SevenKingdoms/static/css/main.b673e3c7.chunk.css"
+  },
+  {
+    "revision": "e4bf918d69cbec6f5f7f",
+    "url": "/SevenKingdoms/static/js/2.b0e53b2c.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/SevenKingdoms/static/js/2.b0e53b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0867639c91b94aedddd6",
+    "url": "/SevenKingdoms/static/js/main.5f3cd16e.chunk.js"
+  },
+  {
+    "revision": "655fc5ae674e7fc5d0be",
+    "url": "/SevenKingdoms/static/js/runtime-main.100567de.js"
+  },
+  {
+    "revision": "d9206504c0c0e98b70056539e7d80463",
+    "url": "/SevenKingdoms/static/media/WhatsApp Image 2020-10-15 at 16.47.44.d9206504.jpeg"
+  },
+  {
+    "revision": "8c3b909fd5ed05140c674858d2b38272",
+    "url": "/SevenKingdoms/static/media/WhatsApp Image 2020-10-15 at 16.47.53.8c3b909f.jpeg"
+  },
+  {
+    "revision": "4539ad1c37db3b9b0850aeb447b54d1f",
+    "url": "/SevenKingdoms/static/media/WhatsApp Image 2020-10-15 at 18.21.39.4539ad1c.jpeg"
+  },
+  {
+    "revision": "4104ddc7c97a1d58139c8faa69f354e1",
+    "url": "/SevenKingdoms/static/media/examenes.4104ddc7.jpg"
+  },
+  {
+    "revision": "2e0a6a7d89f01a01a6213264aa1489a2",
+    "url": "/SevenKingdoms/static/media/fondoPantalla.2e0a6a7d.jpg"
+  },
+  {
+    "revision": "df1314b47a0d11376e01baf858fcdcf7",
+    "url": "/SevenKingdoms/static/media/quiz.df1314b4.jpg"
+  }
+]);
