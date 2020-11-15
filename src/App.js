0a3cc442d9db7f65b1026c2d10e8f4b21ec8afb2
Leandro <soninleandro@gmail.com>
2020-11-15 20:37:45 -0300

    fix github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App" id="home">
         <NavBarra />
         <Routes />
